d660d8e5f158506c19b88edb65508db228187b34
devlooperBR <devlooper9314@gmail.com>
2023-03-14 12:49:16 -0300

    Solução do desafio do dia 2 com o extra

---
+++ scripts/diadois.js
@@ -0,0 +1,17 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+const gosta = prompt("Você gosta de estudar " + linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if(gosta == 1){
+    const opc1 = "Muito bom! Continue estudando e você terá muito sucesso.";
+    alert(opc1);
+}
+else{
+    const opc2 = "Ahh que pena... Já tentou aprender outras linguagens?";
+    alert(opc2);
+}
